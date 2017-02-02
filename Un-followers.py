1d8cd556785da8fa15235b6150db36fb90c95aee
7amza <hamo220058@gmail.com>
2017-02-02 12:56:37 +0300

    Update Un-followers.py

--- Un-followers.py
+++ Un-followers.py
@@ -16,6 +16,8 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+#getting your following List.
 Followed = api.friends_ids('your @name')
 
 while True:
